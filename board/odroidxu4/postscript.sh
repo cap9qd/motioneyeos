65880fc82d4ecb0af0d42ed913a993c8ebb13cf1
Curtis P <33727568+cap9qd@users.noreply.github.com>
2021-01-30 07:54:51 -0600

    Removed boot.ini and postscript.sh changes in odroidxu4

--- board/odroidxu4/postscript.sh
+++ board/odroidxu4/postscript.sh
@@ -15,5 +15,3 @@ cp ${IMG_DIR}/exynos5422-odroidxu4.dtb ${BOOT_DIR}
 cp ${BOARD_DIR}/boot.ini ${BOOT_DIR}
 cp ${BOARD_DIR}/uInitrd ${BOOT_DIR}
 
-cp  ${TARGET}/etc/inittab  ${TARGET}/etc/inittab.bak
-sed -i '/^ttylogin::respawn:\/sbin\/getty -L ttylogin 115200 vt100/a\ttylogin::respawn:\/sbin\/getty -L ttySAC2 115200 vt100'  ${TARGET}/etc/inittab
